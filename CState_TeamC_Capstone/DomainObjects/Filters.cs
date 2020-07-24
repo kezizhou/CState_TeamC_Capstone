63cf3655e821b9710ac06bc7de8ac2b9197bd525
Adam <ajsadowski@cincinnatistate.edu>
2020-07-24 03:27:06 -0400

    Added filter class

---
+++ CState_TeamC_Capstone/DomainObjects/Filters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CState_TeamC_Capstone.DomainObjects
+{
+    public class Filters
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+    }
+}
