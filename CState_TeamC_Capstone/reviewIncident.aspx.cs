5ff300ccb92eabce6965cf375a24f3a1baefe2b9
Adam <ajsadowski@cincinnatistate.edu>
2020-07-25 01:34:54 -0400

    Added drop down selections to review page

--- CState_TeamC_Capstone/reviewIncident.aspx.cs
+++ CState_TeamC_Capstone/reviewIncident.aspx.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using CState_TeamC_Capstone.DomainObjects;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace CState_TeamC_Capstone
-{
-    public partial class reviewIncident : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
+namespace CState_TeamC_Capstone {
+    public partial class reviewIncident : System.Web.UI.Page {
+        public List<Filters> severity;
+        public List<Filters> risk;
+        protected void Page_Load(object sender, EventArgs e) {
+            severity = Shared.GetSeverityFilter();
+            risk = Shared.GetRiskFilter();
+        }
+        protected void Filter(object sender, EventArgs e)
         {
+
+            var departmentSelection = Request["sltDepartment"];
+            var nearMissTypeSElection = Request["sltNearMissType"];
+            var severitySelection = Request["sltSeverityLevel"];
+            var riskSelection = Request["sltRiskLevel"];
+
+            //if (!String.IsNullOrEmpty(departmentSelection))
+            //{
+            //    results = originalResult.Where(x => x.Department == departmentSelection).ToList();
+            //}
+            //if (!String.IsNullOrEmpty(nearMissTypeSElection))
+            //{
+            //    results = originalResult.Where(x => x.NearMissType == nearMissTypeSElection).ToList();
+            //}
+            //if (!String.IsNullOrEmpty(severitySelection))
+            //{
+            //    results = originalResult.Where(x => x.SeverityLevel == severitySelection).ToList();
+            //}
+            //if (!String.IsNullOrEmpty(riskSelection))
+            //{
+            //    results = originalResult.Where(x => x.RiskLevel == riskSelection).ToList();
+            //}
 
         }
     }
